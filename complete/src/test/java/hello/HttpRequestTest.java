0ac3e015874c771e83a9604df2547f1e1fe63cc1
yukihane <yukihane.feather@gmail.com>
2019-04-23 20:39:20 +0900

    明示的な @ExtendWith(SpringExtension.class) 設定は不要

--- complete/src/test/java/hello/HttpRequestTest.java
+++ complete/src/test/java/hello/HttpRequestTest.java
@@ -3,15 +3,12 @@ package hello;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
 
